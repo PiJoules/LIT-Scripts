aee2eae8ff91a366f0bd6d67f25bcb5a9ea8e594
mattygio <vannucc77@msn.com>
2016-03-03 22:44:17 -0500

    Wrote Sensor Code
    Wrote the Sensor Code. The pins are estimates and so are the degrees to
    move the servos.

--- UltraSonic_Sensor/UltraSonic_Sensor.ino
+++ UltraSonic_Sensor/UltraSonic_Sensor.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+Servo myservo;
+int pos;
+int servopin = 6; // Whatever the PWN pin is off the servo
+int digread = 8; //Whatever pin is digital read
+int UP = 90; // Degree to move the servo up
+int DOWN = 180; //Degree to move the servo down
+int Count = 0; //Makes sure it doesn't keep spinning
 
+void setup() {
+  myservo.attach(servopin);
+  pinMode(digread,INPUT);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  val = digitalRead(digread);
+  
+  if(val == HIGH){ // if it detects go up
+    myservo.write(UP);
+    Count++;
+  }else if(val == LOW && Count > 0){ //if it detects go down 
+    myservo.write(DOWN); 
+    Count--;
+  }
+   
+  delay(10);
 
 }
